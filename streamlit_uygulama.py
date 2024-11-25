68f0fa3799b1dd147114bc3c5b9bcefcf614771c
Bugra921 <bugrakoca115@gmail.com>
2024-11-25 21:11:44 +0300

    Update streamlit_uygulama.py

--- streamlit_uygulama.py
+++ streamlit_uygulama.py
@@ -10,7 +10,7 @@ import cv2
 import streamlit as st
 
 
-MODEL = keras.models.load_model("my_model.h5")
+MODEL = keras.models.load_model("my_model.keras")
 CLASS_NAMES = ["angular_leaf_spot", "bean_rust", "healthy"]
 
 def read_file_as_image(data) -> np.ndarray:
